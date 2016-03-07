314e0404ba155950dc968c6d2a9c30c2bac07b41
Rohit <rohitbarnwal7@gmail.com>
2016-03-07 17:00:54 +0530

    title check

--- model.py
+++ model.py
@@ -6,6 +6,8 @@ class Movie(db.Model):
     """Models an individual Movie entry."""
     created = db.DateTimeProperty(auto_now_add=True)
     movie_title = db.StringProperty()
+    title_lower = db.ComputedProperty(lambda self: self.movie_title and self.movie_title.lower())
+
     website = db.StringProperty()
     plot = db.TextProperty()
     cast = db.TextProperty()
@@ -16,7 +18,8 @@ class Movie(db.Model):
     @staticmethod
     def get_movies():
         """
-        Return list of movies added in database ordered by created date, maximum 1000. If database size is large, use pagination.
+        Return list of movies added in database ordered by created date, maximum 1000. 
+        If database size is large, use pagination while fetching from query.
         """
         return Movie.all().filter( "enabled =", True ).order('-created').fetch(1000)
 
@@ -24,9 +27,10 @@ class Movie(db.Model):
     @staticmethod
     def exiting_movie(title):
         """
-        No two movie with same title are allowed to added, add year of release in case of same title or disable the previous one.
+        No two movie with same title are allowed to be added. 
+        Try adding with different title or disable the previous one.
         """
-        movie_object = Movie.all().filter( "movie_title =", title ).get()
+        movie_object = Movie.all().filter( "title_lower =", title ).get()
         if movie_object and movie_object.enabled:
             return True
         return False
