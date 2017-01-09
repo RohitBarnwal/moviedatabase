b4f5d3ff4c372ecaede9d67d1213acc7cf9a3fe5
Rohit <rohitbarnwal7@gmail.com>
2017-01-09 14:27:51 +0530

    now using ndb

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import logging
-
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
-
 import model
-
 import os
 
 
@@ -37,27 +34,152 @@ class HomeHandler(RequestHandler):
 class ListAllMovieHandler(RequestHandler):
     def get(self):
         
-        data = {'movies': model.Movie.get_movies()}
+        movies = model.Movie.get_movies()
+        movies = [(movie.movie_title, movie.key.id()) for movie in movies ]
+        data = {'movies': movies }
         path = os.path.join(os.path.dirname(__file__), 'templates/all_movies.html')
         self.render(path, data)
         
 
 class MovieDetailHandler(RequestHandler):
 
-    def get(self, key):
-        movie = model.Movie.get(key)
-        data = { 'movie': movie }
+    def get(self, movie_id):
+        movie = model.Movie.get_by_id(int(movie_id))
+        data = { 'movie': movie, 'movie_id': movie.key.id(), 'actor_id': movie.actor.id() }
         path = os.path.join(os.path.dirname(__file__), 'templates/movie_detail.html')
         self.render(path, data)
+
+class SearchMovieHandler(RequestHandler):
+    
+    def post(self):
+        
+        title = self.request.get("title")
+        movie = model.Movie.exiting_movie(title.lower())
+        if movie:
+            url = "/detail/{0}/".format(movie.key.id())
+            self.redirect(url)
+        else:
+            data = {'message': "Movie not found"}
+            path = os.path.join(os.path.dirname(__file__), 'templates/main.html')
+            self.render(path, data)
 
 
 class AddNewMovieHandler(RequestHandler):
     
+    def _process(self, message=None, data =None):
+        if data:
+            data.update({ 
+              'message': message,
+            })
+        else:
+            data = {'message': message}
+        path = os.path.join(os.path.dirname(__file__), 'templates/new_movie.html')
+        self.render(path, data)
+
+    def get(self, command=None):
+        actors = model.Actor.get_actors()
+        actors = [(actor.name, actor.key.id()) for actor in actors ]
+        data = {'actors': actors}
+        message = None
+        self._process(message, data)
+
+    def post(self, command):
+        
+        actors = model.Actor.get_actors()
+        actors = [(actor.name, actor.key.id()) for actor in actors ]
+        data = {'actors': actors}
+        if command == 'add':
+            website =  self.request.get("website").replace("http://","").replace("https://","")
+            title = self.request.get("title")
+            movie_exiting = model.Movie.exiting_movie(title.lower())
+            actor_id = self.request.get("actor_id")
+            actor = model.Actor.get_by_id(int(actor_id))
+            if not movie_exiting:
+                
+                imagedata = model.ImageData()
+                imagedata.name = self.request.get('title')
+                imagedata.image = self.request.get('image')
+                imagedata.put()
+                movie = model.Movie(movie_title=title, cast= (self.request.get("cast")), \
+                        plot= (self.request.get("plot")) ,  \
+                        website= website ,enabled=True )
+                movie.actor = actor.key
+                movie.image=imagedata.key
+                movie.put()
+                self._process("The movie has been added successfully.", data)
+            else:
+                self._process('Movie title entered in already existing, change title name if you still want to add it', data)
+        else:
+            self._process("Unsupported command.", data)
+
+
+class AddReviewHandler(RequestHandler):
+    
+    def post(self, movie_id):
+        movie = model.Movie.get_by_id(int(movie_id))
+        review = self.request.get("review")
+        movie.reviews.append(review)
+        movie.put()
+        url = "/detail/{0}/".format(movie_id)
+        self.redirect(url)
+#         data = { 'movie': movie, 'movie_id': movie.key.id(), 'actor_id': movie.actor.id() }
+#         path = os.path.join(os.path.dirname(__file__), 'templates/movie_detail.html')
+#         self.render(path, data)
+
+class ImageHandler(RequestHandler):
+    def get(self, id):
+        movie = model.Movie.get_by_id(int(id))
+        actor = model.Actor.get_by_id(int(id))
+        if movie and movie.image:
+            self.response.headers['Content-Type'] = "image/png"
+            self.response.out.write(movie.image.get().image)
+        elif actor and actor.image:
+            self.response.headers['Content-Type'] = "image/png"
+            self.response.out.write(actor.image.get().image)
+
+        else:
+            self.error(404)
+
+
+class DisableMovieHandler(RequestHandler):
+
+    def get(self, movie_id):
+        movie = model.Movie.get_by_id(int(movie_id))
+        
+        if not movie:
+            self.error(404)
+        movie.enabled = False
+        movie.put()
+        self.redirect('/')
+
+
+class ListAllActorHandler(RequestHandler):
+    def get(self):
+        
+        actors = model.Actor.get_actors()
+        actors = [(actor.name, actor.key.id()) for actor in actors ]
+        data = {'actors': actors }
+        path = os.path.join(os.path.dirname(__file__), 'templates/all_actors.html')
+        self.render(path, data)
+
+class ActorDetailHandler(RequestHandler):
+
+    def get(self, actor_id):
+        actor = model.Actor.get_by_id(int(actor_id))
+        movies = model.Movie.get_by_actor(actor.key)
+        movies = [(movie.movie_title, movie.key.id()) for movie in movies ]
+        data = {'movies': movies, 'actor': actor, 'actor_id': actor_id}
+        path = os.path.join(os.path.dirname(__file__), 'templates/actor_detail.html')
+        self.render(path, data)
+
+
+class AddActorHandler(RequestHandler):
+    
     def _process(self, message=None):
         data = { 
-          'message': message,
+            'message': message,
         }
-        path = os.path.join(os.path.dirname(__file__), 'templates/new_movie.html')
+        path = os.path.join(os.path.dirname(__file__), 'templates/new_actor.html')
         self.render(path, data)
 
     def get(self, command=None):
@@ -67,44 +189,35 @@ class AddNewMovieHandler(RequestHandler):
 
         if command == 'add':
             website =  self.request.get("website").replace("http://","").replace("https://","")
-            title = self.request.get("title")
-            movie_exiting = model.Movie.exiting_movie(title.lower())
-            if not movie_exiting:
-                image = self.request.get("image")
-                image = db.Blob(image) if image else None
+            name = self.request.get("name")
+            actor_exiting = model.Actor.get_by_name(name.lower())
+            if not actor_exiting:
                 
-                movie = model.Movie(movie_title=title, cast= (self.request.get("cast")), \
-                        plot= (self.request.get("plot")) , image=image, \
+                imagedata = model.ImageData()
+                imagedata.name = self.request.get('name')
+                imagedata.image = self.request.get('image')
+                imagedata.put()
+                actor = model.Actor(name=name, about= (self.request.get("about")), \
                         website= website ,enabled=True )
-                movie.put()
-                self._process("The movie has been added successfully.")
-            else:
-                self._process('Movie title entered in already existing, change title name if you still want to add it')
+                actor.image=imagedata.key
+                actor.put()
+            self._process("The actor has been added successfully.")
+#             else:
+#                 self._process('Movie title entered in already existing, change title name if you still want to add it')
         else:
             self._process("Unsupported command.")
-
-
-class ImageHandler(RequestHandler):
-    def get(self, id):
-        movie = db.get(id)
-        if movie.image:
-            self.response.headers['Content-Type'] = "image/png"
-            self.response.out.write(movie.image)
-        else:
-            self.error(404)
-
-
-class DisableMovieHandler(RequestHandler):
-
-    def get(self, key):
-        movie = model.Movie.get(key)
+            
+class GivenActorMovies(RequestHandler):
+    
+    def get(self, actor_id):
         
-        if not movie:
-            self.error(404)
-        movie.enabled = False
-        movie.put()
-        self.redirect('/')
-
+        actor = model.Actor.get_by_id(int(actor_id)).key
+        movies = model.Movie.get_by_actor(actor)
+        movies = [(movie.movie_title, movie.key.id()) for movie in movies ]
+        data = {'movies': movies}
+        path = os.path.join(os.path.dirname(__file__), 'templates/all_movies.html')
+        self.render(path, data)
+        
         
 class NotFound(RequestHandler):
     def get(self):
@@ -117,6 +230,13 @@ app = webapp.WSGIApplication( [
     ('/new', AddNewMovieHandler),
     ('/new/(.*)/', AddNewMovieHandler),
     ('/detail/(.*)/', MovieDetailHandler),
+    ('/addreview/(.*)/', AddReviewHandler),
+    ('/listactors', ListAllActorHandler),
+    ('/newactor', AddActorHandler),
+    ('/newactor/(.*)/', AddActorHandler),
+    ('/givenactormovie/(.*)/', GivenActorMovies),
+    ('/actordetail/(.*)/', ActorDetailHandler),
+    ('/searchmovie', SearchMovieHandler),
     ('/image/(.*)/', ImageHandler),
     ('/disable/(.*)/', DisableMovieHandler),
     ('/.*', NotFound),
--- model.py
+++ model.py
@@ -1,27 +1,28 @@
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 
 
-class Movie(db.Model):
+class Movie(ndb.Model):
     
     """Models an individual Movie entry."""
-    created = db.DateTimeProperty(auto_now_add=True)
-    movie_title = db.StringProperty()
-    title_lower = db.ComputedProperty(lambda self: self.movie_title and self.movie_title.lower())
-
-    website = db.StringProperty()
-    plot = db.TextProperty()
-    cast = db.TextProperty()
-    image = db.BlobProperty()
-    enabled = db.BooleanProperty()
-    
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    movie_title = ndb.StringProperty()
+    title_lower = ndb.ComputedProperty(lambda self: self.movie_title and self.movie_title.lower())
+    actor = ndb.KeyProperty()
+    website = ndb.StringProperty()
+    plot = ndb.TextProperty()
+    cast = ndb.TextProperty()
+    image = ndb.KeyProperty()
+    enabled = ndb.BooleanProperty()
+    reviews = ndb.StringProperty(repeated=True)
 
     @staticmethod
     def get_movies():
         """
-        Return list of movies added in database ordered by created date, maximum 1000. 
+        Return list of movies added in database ordered by created date.
         If database size is large, use pagination while fetching from query.
         """
-        return Movie.all().filter( "enabled =", True ).order('-created').fetch(1000)
+#         return Movie.all().filter( "enabled =", True ).order('-created').fetch(1000)
+        return Movie.query(Movie.enabled==True).fetch()
 
 
     @staticmethod
@@ -30,7 +31,46 @@ class Movie(db.Model):
         No two movie with same title are allowed to be added. 
         Try adding with different title or disable the previous one.
         """
-        movie_object = Movie.all().filter( "title_lower =", title ).get()
+        movie_object = Movie.query(Movie.title_lower == title).get()
         if movie_object and movie_object.enabled:
-            return True
-        return False
+            return movie_object
+        return None
+
+    @staticmethod
+    def get_by_actor(actor_key):
+        
+        movies = Movie.query(Movie.actor==actor_key).fetch()
+        
+        return movies
+
+class ImageData(ndb.Model):
+     name = ndb.StringProperty(indexed=False)
+     image = ndb.BlobProperty()
+
+
+class Actor(ndb.Model):
+    
+    """Models an actor entry."""
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    name = ndb.StringProperty()
+    name_lower = ndb.ComputedProperty(lambda self: self.name and self.name.lower())
+    website = ndb.StringProperty()
+    about = ndb.TextProperty()
+    image = ndb.KeyProperty()
+    enabled = ndb.BooleanProperty()
+    
+    @staticmethod
+    def get_by_name(name):
+       
+        actor = Actor.query().filter(Actor.name_lower == name).get()
+        if actor and actor.enabled:
+            return actor
+        return None
+    
+    @staticmethod
+    def get_actors():
+        """
+        Return list of actors added in database ordered by created date.
+        If database size is large, use pagination while fetching from query.
+        """
+        return Actor.query(Actor.enabled==True).fetch()
